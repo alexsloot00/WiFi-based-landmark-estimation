f699d0add1c661d406cf01a94e89f60496359942
alexsloot00 <118809255+alexsloot00@users.noreply.github.com>
2023-01-19 16:15:17 +0100

    Create main.py

---
+++ python_on_robot/main.py
@@ -0,0 +1,13 @@
+"""
+This file is yet to be created. 
+It will contain the following:
+ - movement commands for the robot
+ - decision when to start collecting CSI data
+ - function calls to send the collected data to a separate computing unit
+"""
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
