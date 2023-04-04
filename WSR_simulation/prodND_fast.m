bd0727f14f138afbe8c922e43d11418eb7bc14e3
alexsloot00 <118809255+alexsloot00@users.noreply.github.com>
2023-04-04 14:22:12 +0200

    Delete WSR_simulation directory

--- WSR_simulation/prodND_fast.m
+++
@@ -1,6 +0,0 @@
-function result = prodND_fast(leftMat, rightMat)
-    % (2022) Original code: Ninad Jadhav - Harvard University
-    lM = permute(leftMat, [2, 3, 1]);
-    rM = repmat(rightMat, [1, 1, size(leftMat, 1)]);
-    result = mtimesx(lM, rM, 'SPEED');
-end
